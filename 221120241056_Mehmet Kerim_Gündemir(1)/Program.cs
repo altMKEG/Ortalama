156940d1ef52e4a8bc5aa45bb3f34ed62644c1fc
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-21 01:54:44 +0300

    Proje dosyası ekle.

---
+++ 221120241056_Mehmet Kerim_Gündemir(1)/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("3 dersin ortalaması");
+int lesson1;
+int lesson2;
+int lesson3;
+
+
+Console.Write("Ders 1 notunu girin -> ");
+lesson1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ders 2 notunu girin -> ");
+lesson2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ders 3 notunu girin ->");
+lesson3 = Convert.ToInt32(Console.ReadLine());
+
+int average = (lesson1 + lesson2 + lesson3) / 3;
+
+Console.WriteLine($"Ortalamanız -> {average}");
+if (average >= 50)
+{     
+    Console.WriteLine("Başarıyla Geçtiniz"); 
+}
+else
+{
+    Console.WriteLine("Kaldınız");
+}
+Console.ReadLine();
+
+
+
